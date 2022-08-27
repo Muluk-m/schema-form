b0d6a57cd31686108ccb9c87388b806f601bf9ee
maqiqian <1363964820@qq.com>
2022-08-27 18:33:16 +0800

    chore: bump dependencies

--- packages/vue3-example/vite.config.ts
+++ packages/vue3-example/vite.config.ts
@@ -3,16 +3,15 @@ import { defineConfig } from 'vite';
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
     },
+    preserveSymlinks: true,
   },
   plugins: [
-    vueJsx(),
     vue(),
     Components({
       resolvers: [VantResolver()],
