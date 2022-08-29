106de3eaa75e569f9d8550b4c37b86b18ed157ba
maqiqian <1363964820@qq.com>
2022-08-29 09:49:48 +0800

    docs: update assets base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
-  base: '/',
+  base: '/schema-form',
   title: 'Vue3 Schema Form',
   description: 'Example of configured options of VitePress',
   lastUpdated: true,
