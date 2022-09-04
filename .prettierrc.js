1007f8a9211410878a95dd58bad2b57e56f15a56
maqiqian <1363964820@qq.com>
2022-09-04 20:30:14 +0800

    chore: adjust code style

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-  printWidth: 100,
+  printWidth: 80,
   tabWidth: 2,
   useTabs: false,
   semi: true,
   singleQuote: true,
   jsxSingleQuote: true,
   // trailingComma: 'none',
+  singleAttributePerLine: true,
   bracketSpacing: true,
   jsxBracketSameLine: false,
   arrowParens: 'always',
